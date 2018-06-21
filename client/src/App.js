28e4f60dc894e2d31d70cc38c3f652fb683d7015
Wood Jerry Remelus <woodjremelus@gmail.com>
2018-06-20 19:10:13 -0600

    done

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Main from "./pages/Main";
+import NoMatch from "./pages/NoMatch";
+import Nav from "./components/Nav";
+import { Container } from "./components/Grid";
+
+const App = () =>
+  <Router>
+    <div className="container">
+      <Nav />
+      <Container fluid>
+            <Switch>
+              <Route exact path="/" component={Main} />
+              <Route exact path="/main" component={Main} />
+              <Route exact path="/main/:id" component={Main} />
+              <Route component={NoMatch} />
+            </Switch>
+      </Container>
+      <footer className="footer text-center">
+        <p className="text-muted small mb-0">Copyright &copy; </p>
+      </footer>
+    </div>
+  </Router>;
+
+export default App;
---
+++ client/src/components/Nav/Nav.js
@@ -0,0 +1,16 @@
+
+import React from "react";
+import "./Nav.css";
+
+
+const Nav = () =>
+  <div className="jumbotron jumbo">
+    <h1 className="text-center">
+      <strong>
+        <i className="fa fa-newspaper-o"></i> 
+        New York Times Article Search
+      </strong>
+    </h1>
+  </div>;
+
+export default Nav;
